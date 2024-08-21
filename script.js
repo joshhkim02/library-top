91d3996e8a8a347382cb529796f2324849603138
joshhkim02 <joshhkim02@gmail.com>
2024-08-20 17:36:14 -0700

    feat: finished add book functionality

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const myLibrary = [];
+
 const main = document.querySelector('.main');
 const dialog = document.querySelector('dialog');
 const addButton = document.querySelector('.btn-add');
-const closeButton = document.querySelector('dialog button');
-    
-function Book(author, title, pages, isRead) {
-    this.author = author;
+const closeButton = document.querySelector('.btn-close');
+const submitButton = document.querySelector('.btn-submit');
+
+const titleInput = document.querySelector('#book_title');
+const authorInput = document.querySelector('#author_name');
+const pagesInput = document.querySelector('#num_pages');
+const readInput = document.querySelector('#is_read');
+
+function Book(title, author, pages, isRead) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 }
@@ -27,6 +34,35 @@ function displayLibrary(library) {
     }
 }
 
+submitButton.addEventListener('click', (event) => {
+    // Get user input
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read =  readInput.value;
+
+    // Create and add new book to DOM tree
+    let newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+    const createBook = document.createElement('div');
+    createBook.classList.add('card');
+    createBook.textContent = `${newBook.title}
+                              ${newBook.author}
+                              ${newBook.pages}
+                              ${newBook.isRead}`;
+    main.appendChild(createBook);
+
+    // Reset input values in form 
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.value = "";
+
+    // Prevent trying to send data to server and close the dialog box
+    event.preventDefault();
+    dialog.close();
+});
+
 addButton.addEventListener('click', () => {
     dialog.showModal();
 })
@@ -35,14 +71,12 @@ closeButton.addEventListener('click', () => {
     dialog.close();
 })
 
-const book1 = new Book('Harry Potter', 'Spells', 93, 'yes');
-const book2 = new Book('Gandalf', 'Hobbits', 183, 'no');
-const book3 = new Book('Legolas', 'Bows', 132, 'yes');
+const book1 = new Book('Fahrenheit 451', 'Ray Bradbury', 192, 'Yes');
+const book2 = new Book('Flowers for Algernon', 'Daniel keyes', 311, 'Yes');
+const book3 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 208, 'No');
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
-
-console.log(myLibrary);
 
 displayLibrary(myLibrary);
