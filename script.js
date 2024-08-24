444f8a0d37b2bff4b1638e48da7baac18b677ee4
joshhkim02 <joshhkim02@gmail.com>
2024-08-23 18:00:59 -0700

    fix: improved delete book functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let increment = 0;
 
 const main = document.querySelector('.main');
 const dialog = document.querySelector('dialog');
@@ -12,11 +13,13 @@ const authorInput = document.querySelector('#author_name');
 const pagesInput = document.querySelector('#num_pages');
 const readInput = document.querySelector('#is_read');
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, index = increment, deleted = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.index = index;
+    this.deleted = deleted;
 }
 
 function resetInput() {
@@ -30,46 +33,48 @@ function addBookToLibrary(book) {
     return myLibrary.push(book);
 }
 
-function removeBookFromLibrary(id) {
-    return myLibrary.splice(id, 1);
-}
 
 function displayLibrary(library) {
     for (const book of library) {
-        const createBook = document.createElement('div');
-        createBook.classList.add('card');
-        createBook.textContent = `Title: ${book.title}
-                                  Author: ${book.author}
-                                  Pages: ${book.pages}
-                                  Has read: ${book.isRead}`;
-        main.appendChild(createBook);
-
-        // Create container to hold buttons
-        const bookContainer = document.createElement('div');
-        bookContainer.classList.add('book-btn-container');
-        createBook.appendChild(bookContainer);
-
-        // Add delete and read buttons
-        const deleteBook = document.createElement('button');
-        const readBook = document.createElement('button');
-
-        deleteBook.classList.add('book-btn', 'delete-book');
-        readBook.classList.add('book-btn', 'read-book');
-
-        deleteBook.textContent = 'Remove';
-        readBook.textContent = 'Finished';
-
-        bookContainer.appendChild(deleteBook);
-        bookContainer.appendChild(readBook);
-
-        deleteBook.addEventListener('click', () => {
-            removeBookFromLibrary(myLibrary.indexOf(book));
-            main.removeChild(createBook);
-        })
-
-        readBook.addEventListener('click', () => {
-            console.log('whatup doe');
-        })
+        if (book.deleted != true) {
+            const createBook = document.createElement('div');
+            createBook.classList.add('card');
+            createBook.textContent = `Title: ${book.title}
+                                      Author: ${book.author}
+                                      Pages: ${book.pages}
+                                      Has read: ${book.isRead}`;
+            main.appendChild(createBook);
+    
+            // Create container to hold buttons
+            const bookContainer = document.createElement('div');
+            bookContainer.classList.add('book-btn-container');
+            createBook.appendChild(bookContainer);
+    
+            // Add delete and read buttons
+            const deleteBook = document.createElement('button');
+            const readBook = document.createElement('button');
+    
+            deleteBook.classList.add('book-btn', 'delete-book');
+            readBook.classList.add('book-btn', 'read-book');
+    
+            deleteBook.textContent = 'Remove';
+            readBook.textContent = 'Finished';
+    
+            bookContainer.appendChild(deleteBook);
+            bookContainer.appendChild(readBook);    
+    
+            deleteBook.addEventListener('click', () => {
+                book.deleted = true;
+                main.removeChild(createBook);
+            })
+    
+            readBook.addEventListener('click', () => {
+                console.log('whatup doe');
+            })
+        }
+        else {
+            continue;
+        }
     }
 }
 
@@ -80,8 +85,9 @@ submitButton.addEventListener('click', (event) => {
     let pages = pagesInput.value;
     let read =  readInput.value;
 
-    // Create and add new book to DOM tree
+    // Create new book
     let newBook = new Book(title, author, pages, read);
+    increment++;
     addBookToLibrary(newBook);
 
     resetInput();
